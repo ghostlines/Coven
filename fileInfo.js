59985b2c5d2bf9b77e40cbf72d09080800d8bdeb
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2017-03-15 12:55:38 -0700

    Load new page on file open #3

--- fileInfo.js
+++ fileInfo.js
@@ -3,24 +3,12 @@ const path = require('path')
 const url = require('url')
 
 const FileInfo = {
-  openWindow: function () {
-    // const filepath = process.argv[2]
-
-    win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      icon: path.join(__dirname, 'assets/icons/mac/defconAppKitUFOIcon.icns')
-    })
-
-    win.loadURL(url.format({
+  openWindow: function (mainWindow) {
+    mainWindow.loadURL(url.format({
       pathname: path.join(__dirname, 'fileInfo.html'),
       protocol: 'file:',
       slashes: true
     }))
-
-    win.on('closed', () => {
-      win = null
-    })
   }
 }
 
--- main.js
+++ main.js
@@ -1,49 +1,26 @@
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const path = require('path')
 const url = require('url')
 const FileInfo = require('./fileInfo.js')
-let win
 
-// getting the filepath through a argument passed when running `electron . filepath` for testing purposes
+let mainWindow
 
-function createWindow () {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: path.join(__dirname, 'assets/icons/mac/defconAppKitUFOIcon.icns')
-  })
+app.on('open-file', (event, filepath) => {
+  event.preventDefault();
+  fileToOpen = filepath;
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  win.webContents.openDevTools()
-
-  win.on('closed', () => {
-    win = null
-  })
-}
-
-// app.on('will-finish-launching', () => {
-  app.on('open-file', (event, path) => {
-    event.preventDefault()
-    FileInfo.openWindow
-  })
-// })
-
-// this is for testing purposes. not sure what should be the default if app is not opened through a double-click
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit()
+  if (mainWindow) {
+    FileInfo.openWindow(mainWindow);
   }
-})
+});
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
-})
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+});
