02a93bd88101138b379a62836bed164fa0977c10
Jack Jennings <jack@standard-library.com>
2017-07-14 17:26:23 -0700

    Gracefully open window

--- main-process/file.js
+++ main-process/file.js
@@ -1,5 +1,6 @@
 import { BrowserWindow } from "electron";
 import fs from "fs";
+import Kefir from "kefir";
 import path from "path";
 import url from "url";
 
@@ -7,9 +8,10 @@ const File = {
   display: function(filePath) {
     const infoPlistData = fs.readFileSync(`${filePath}/info.plist`);
     const documentsPlistData = fs.readFileSync(`${filePath}/documents.plist`);
-    const mainWindow = new BrowserWindow({ width: 600, height: 400 });
+    const window = new BrowserWindow({ width: 600, height: 400, show: false });
+    const windowReady = Kefir.fromEvents(window, "ready-to-show").take(1);
 
-    mainWindow.loadURL(
+    window.loadURL(
       url.format({
         pathname: path.join(__dirname, "../file.html"),
         protocol: "file:",
@@ -17,13 +19,15 @@ const File = {
       })
     );
 
-    mainWindow.webContents.on("did-finish-load", () => {
-      mainWindow.webContents.send("ping", {
+    window.webContents.on("did-finish-load", () => {
+      window.webContents.send("ping", {
         infoPlist: infoPlistData.toString(),
         documentsPlist: documentsPlistData.toString(),
         fileName: path.basename(filePath)
       });
     });
+
+    windowReady.observe(() => window.show());
   }
 };
 
