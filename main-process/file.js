35fd262718a6713a370d55b4fc8c328d1328f7cd
Jack Jennings <jack@standard-library.com>
2017-07-17 09:02:01 -0700

    Clear browser window from memory after cloing

--- main-process/file.js
+++ main-process/file.js
@@ -9,20 +9,23 @@ const File = {
   display: function(filePath) {
     const ufs = readUFS(filePath);
     const fileName = path.basename(filePath);
-    const window = new BrowserWindow({ width: 600, height: 400, show: false });
-    const windowReady = Kefir.fromEvents(window, "ready-to-show").take(1);
-    const windowLoaded = Kefir.fromEvents(
-      window.webContents,
-      "did-finish-load"
-    );
 
-    windowLoaded.observe(() => {
-      window.webContents.send("ping", { ufs, fileName });
+    let ui = new BrowserWindow({ width: 600, height: 400, show: false });
+    const uiReady = Kefir.fromEvents(ui, "ready-to-show").take(1);
+    const uiClosed = Kefir.fromEvents(ui, "closed");
+    const uiLoaded = Kefir.fromEvents(ui.webContents, "did-finish-load");
+
+    uiLoaded.observe(() => {
+      ui.webContents.send("ping", { ufs, fileName });
     });
 
-    windowReady.observe(() => window.show());
+    uiReady.observe(() => ui.show());
 
-    window.loadURL(
+    uiClosed.observe(() => {
+      ui = null;
+    });
+
+    ui.loadURL(
       url.format({
         pathname: path.join(__dirname, "../file.html"),
         protocol: "file:",
