4bdbb2db623c99fbcc7560f3a1c1a55c4cf23b74
Jack Jennings <jack@standard-library.com>
2017-07-17 08:37:34 -0700

    Use streams in file handler

--- main-process/file.js
+++ main-process/file.js
@@ -8,8 +8,19 @@ import { read as readUFS } from "./ufs";
 const File = {
   display: function(filePath) {
     const ufs = readUFS(filePath);
+    const fileName = path.basename(filePath);
     const window = new BrowserWindow({ width: 600, height: 400, show: false });
     const windowReady = Kefir.fromEvents(window, "ready-to-show").take(1);
+    const windowLoaded = Kefir.fromEvents(
+      window.webContents,
+      "did-finish-load"
+    );
+
+    windowLoaded.observe(() => {
+      window.webContents.send("ping", { ufs, fileName });
+    });
+
+    windowReady.observe(() => window.show());
 
     window.loadURL(
       url.format({
@@ -18,14 +29,6 @@ const File = {
         slashes: true
       })
     );
-
-    window.webContents.on("did-finish-load", () => {
-      const fileName = path.basename(filePath);
-
-      window.webContents.send("ping", { ufs, fileName });
-    });
-
-    windowReady.observe(() => window.show());
   }
 };
 
