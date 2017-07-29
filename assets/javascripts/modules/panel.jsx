678e38dfe7916da860fc6acc130d82e904a434db
Jack Jennings <jack@standard-library.com>
2017-07-29 09:58:47 -0700

    Parse plist files

--- assets/javascripts/modules/panel.jsx
+++ assets/javascripts/modules/panel.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+function format(content) {
+  if (typeof content === "string") {
+    return content;
+  } else {
+    return JSON.stringify(content);
+  }
+}
+
 class Panel extends React.Component {
   render() {
+    const content = format(this.props.content);
+
     return (
       <div className="panel">
         <div className="panel__content">
-          {this.props.content}
+          {content}
         </div>
       </div>
     );
--- main-process/ufs.js
+++ main-process/ufs.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
+const plist = require("fast-plist");
 
 function readDocument(basePath, fileName) {
   return fs.readFileSync(`${basePath}/${fileName}`).toString();
 }
 
+function readPlist(basePath, fileName) {
+  return plist.parse(readDocument(basePath, fileName));
+}
+
 function read(filePath) {
-  const documents = readDocument(filePath, "documents.plist");
+  const documents = readPlist(filePath, "documents.plist");
   const features = readDocument(filePath, "features.fea");
-  const fontinfo = readDocument(filePath, "fontinfo.plist");
-  const groups = readDocument(filePath, "groups.plist");
+  const fontinfo = readPlist(filePath, "fontinfo.plist");
+  const groups = readPlist(filePath, "groups.plist");
   const interpolation = readDocument(filePath, "interpolation.designspace");
-  const lib = readDocument(filePath, "lib.plist");
-  const metainfo = readDocument(filePath, "metainfo.plist");
+  const lib = readPlist(filePath, "lib.plist");
+  const metainfo = readPlist(filePath, "metainfo.plist");
 
   return {
     documents,
